c42239056b67079283534351f8cc51be6449a41e
Levi Broderick <levib@microsoft.com>
2018-06-22 15:37:18 -0700

    Clean up Utf8 method patterns

---
+++ Utf8Apis/InvalidSequenceBehavior.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System.Text
+{
+    /// <summary>
+    /// Describes the remediation that the text processor should take when it
+    /// encounters an invalid sequence in the input stream.
+    /// </summary>
+    public enum InvalidSequenceBehavior
+    {
+        /// <summary>
+        /// The text processor should report failure and should not attempt to proceed
+        /// past the invalid sequence.
+        /// </summary>
+        Fail,
+
+        /// <summary>
+        /// The text processor should treat the invalid sequence as if it had instead seen
+        /// the scalar value U+FFFD, and the text processor should attempt to continue.
+        /// </summary>
+        SubstituteReplacementChar,
+    }
+}
--- Utf8Apis/Utf8.cs
+++ Utf8Apis/Utf8.cs
@@ -9,25 +9,109 @@ namespace System.Text
     // Alternative naming: class Utf8Convert in namespace System.Text.Utf8
     public static partial class Utf8
     {
+        /*
+         * TRANSCODING ROUTINES
+         */
+
         // Converts UTF8 -> UTF16.
-        // Similar to Utf8Encoder.Convert, but uses OperationStatus-style APIs.
+        // Assumes the input is a standalone UTF-8 string.
+        // Returns number of output chars written.
+        // Throws on failure (malformed input, destination too small).
+        public static int ConvertToUtf16(
+            ReadOnlySpan<byte> inUtf8,
+            Span<char> outUtf16,
+            InvalidSequenceBehavior invalidSequenceBehavior = InvalidSequenceBehavior.Fail) => throw null;
+
+        // A non-throwing form of ConvertToUtf16.
+        // Follows normal OperationStatus-style conventions.
         public static OperationStatus ConvertToUtf16(
-            ReadOnlySpan<Char8> input,
-            Span<char> output,
-            out int elementsConsumed,
-            out int elementsWritten,
-            InvalidSequenceBehavior replacementBehavior = InvalidSequenceBehavior.ReplaceInvalidSequence,
+            ReadOnlySpan<byte> inUtf8,
+            Span<char> outUtf16,
+            out int bytesConsumed,
+            out int charsWritten,
+            InvalidSequenceBehavior invalidSequenceBehavior = InvalidSequenceBehavior.Fail,
             bool isFinalChunk = true) => throw null;
 
-        // Converts UTF16 -> UTF8.
-        // Similar to Utf8Encoder.Convert, but uses OperationStatus-style APIs.
+        // Converts to UTF-16 and returns a new System.String instance
+        // rather than writing to a span-based output buffer.
+        public static string ConvertToString(
+            ReadOnlySpan<byte> inUtf8,
+            InvalidSequenceBehavior invalidSequenceBehavior = InvalidSequenceBehavior.Fail) => throw null;
+
+        // Converts UTF-16 -> UTF-8.
+        // Assumes the input is a standalone UTF-16 string.
+        // Returns number of output bytes written.
+        // Throws on failure (malformed input, destination too small).
+        public static int ConvertFromUtf16(
+            ReadOnlySpan<char> inUtf16,
+            Span<byte> outUtf8,
+            InvalidSequenceBehavior invalidSequenceBehavior = InvalidSequenceBehavior.Fail) => throw null;
+
+        // A non-throwing form of ConvertToUtf16.
+        // Follows normal OperationStatus-style conventions.
         public static OperationStatus ConvertFromUtf16(
-            ReadOnlySpan<char> input,
-            Span<Char8> output,
-            out int elementsConsumed,
-            out int elementsWritten,
-            InvalidSequenceBehavior replacementBehavior = InvalidSequenceBehavior.ReplaceInvalidSequence,
+            ReadOnlySpan<char> inUtf16,
+            Span<byte> outUtf8,
+            out int charsConsumed,
+            out int bytesWritten,
+            InvalidSequenceBehavior invalidSequenceBehavior = InvalidSequenceBehavior.Fail,
             bool isFinalChunk = true) => throw null;
+
+        /*
+         * COMPARISON ROUTINES
+         */
+
+        // Returns true iff two UTF-8 strings are equal.
+        // Caller can specify comparison mode (ordinal, case-sensitive, culture-aware).
+        // Two malformed UTF-8 strings will only ever compare as equal using an ordinal comparer.
+        public static bool AreEqual(
+            ReadOnlySpan<byte> a,
+            ReadOnlySpan<byte> b,
+            StringComparison stringComparison = StringComparison.Ordinal) => throw null;
+
+        // Returns true iff the specified UTF-8 and UTF-16 strings are equal.
+        // Caller can specify comparison mode (ordinal, case-sensitive, culture-aware).
+        // Returns false if either input string is malformed.
+        public static bool AreEqual(
+            ReadOnlySpan<byte> a,
+            ReadOnlySpan<char> b,
+            StringComparison stringComparison = StringComparison.Ordinal) => throw null;
+
+        // Returns a comparison between two UTF-8 strings: negative if a should be sorted
+        // before b, zero if a and b are equal, and positive if a should be sorted after b.
+        // Caller can specify comparison mode (ordinal, case-sensitive, culture-aware).
+        public static int Compare(
+            ReadOnlySpan<byte> a,
+            ReadOnlySpan<byte> b,
+            StringComparison stringComparison = StringComparison.Ordinal) => throw null;
+
+        /*
+         * TRIM ROUTINES
+         */
+
+        // Trims whitespace characters from the beginning and end of the input.
+        // Returns a slice of the original input.
+        public static ReadOnlySpan<byte> Trim(
+            ReadOnlySpan<byte> inUtf8) => throw null;
+
+        // Trims the requested scalar value from the beginning and end of the input.
+        // Returns a slice of the original input.
+        public static ReadOnlySpan<byte> Trim(
+            ReadOnlySpan<byte> inUtf8,
+            UnicodeScalar trimScalar) => throw null;
+
+        // Trims the requested scalar values from the beginning and end of the input.
+        // Returns a slice of the original input.
+        public static ReadOnlySpan<byte> Trim(
+            ReadOnlySpan<byte> inUtf8,
+            ReadOnlySpan<UnicodeScalar> trimScalars) => throw null;
+
+        // There's no API to get the number of bytes trimmed. Instead, call TrimStart / TrimEnd
+        // and compare the length of the returned string against the length of the input string,
+        // and this will give the number of bytes trimmed from the start + end.
+        //
+        // (Assume there are TrimStart / TrimEnd equivalents of Trim.)
+
 
         // Converts UTF8 -> UTF8, fixing up invalid sequences along the way.
         // (This will never return InvalidData.)
@@ -55,12 +139,6 @@ namespace System.Text
         public static ReadOnlyMemory<Char8> ConvertToValidUtf8(ReadOnlyMemory<Char8> input) => throw null;
     }
 
-    public enum InvalidSequenceBehavior
-    {
-        ReplaceInvalidSequence, // replace invalid sequences with U+FFFD
-        Fail, // don't repalce invalid sequences; report failure instead
-    }
-
     // Contains helper methods for inspecting UTF-8 data.
     // Alternative naming: class Utf8Inspection in namespace System.Text.Utf8
     public static partial class Utf8
@@ -86,7 +164,7 @@ namespace System.Text
             ReadOnlySpan<Char8> utf8Text,
             out int utf16CharCount,
             out int scalarCount,
-            InvalidSequenceBehavior replacementBehavior = InvalidSequenceBehavior.ReplaceInvalidSequence) => throw null;
+            InvalidSequenceBehavior replacementBehavior = InvalidSequenceBehavior.Fail) => throw null;
 
         // Given UTF-16 input text, returns the number of UTF-8 characters and the
         // number of scalars present. Returns false iff the input sequence is invalid
@@ -97,7 +175,7 @@ namespace System.Text
             ReadOnlySpan<char> utf16Text,
             out int utf8CharCount,
             out int scalarCount,
-            InvalidSequenceBehavior replacementBehavior = InvalidSequenceBehavior.ReplaceInvalidSequence) => throw null;
+            InvalidSequenceBehavior replacementBehavior = InvalidSequenceBehavior.Fail) => throw null;
 
         // Returns the index of the first invalid UTF-8 sequence, or -1 if the input text
         // is well-formed UTF-8. Additionally returns the number of UTF-16 chars and the
